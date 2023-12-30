97c15b47c2796dbcc98fd80ddd131dd2c7fa92bc
mammukb <muhammedkb156@gmail.com>
2023-12-30 10:05:29 +0530

    Skills added and about me completed

--- src/app/Components/skills.js
+++ src/app/Components/skills.js
@@ -1,39 +1,42 @@
-"use client"
-
 import React from "react";
 import Image from "next/image";
 
-function skills() {
- /* // Number of images
+function Skills() {
+  // Number of images
   const numberOfImages = 14;
-  const testImagePath = `${process.env.PUBLIC_URL}/images/image1.png`;
-  console.log("Test Image Path:", testImagePath);
 
   // Generate an array of image paths
-  const imageArray = Array.from({ length: numberOfImages }, (_,index) => {
+  const imageArray = Array.from({ length: numberOfImages }, (_, index) => {
     const imageNumber = index + 1;
-    return `../../../public/images/image${imageNumber}.png`;
-  });*/
-
-  //
+    return `/images/image${imageNumber}.png`; // Use relative path
+  });
 
   return (
     <>
       <div className="flex lg:flex-row sm:flex-col">
-        <div className="text-maincolor-50 sm:m-11 lg:m-20 ">
-          <h1 className="lg:text-8xl  font-extrabold  sm:text-6xl   ">
+        <div className="text-maincolor-50 sm:m-11 lg:m-20">
+          <h1 className="lg:text-8xl font-extrabold sm:text-6xl">
             Things <br /> I Know ??
           </h1>
-          <p className="text-sm"> Familiar with using these technologies</p>
+          <p className="text-sm">Familiar with using these technologies</p>
         </div>
-        <div>
-          {/*imageArray.map((imageUrl, index) => (
-            <Image key={index} src={imageUrl} alt={`Image ${index + 1}`}   width={100} height={100}   />
-          ))*/}
+        <div  className="flex flex-row gap-8 flex-wrap items-center sm:m-14  "  >
+          {imageArray.map((imageUrl, index) => (
+            <Image
+              key={index}
+              src={imageUrl}
+              alt={`Image ${index + 1}`}
+              width={100}
+              height={100}
+               className="circularImage    "
+            />
+          ))}
         </div>
       </div>
     </>
   );
 }
 
-export default skills;
+export default Skills;
+
+
